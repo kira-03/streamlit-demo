79a65666f8864657c8c422c013b3e927fa70e632
kira-03 <guruprasathmaheswaran.07@gmail.com>
2024-07-24 18:37:48 +0530

    pokemon try

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 
-# Create a title
-st.title("My First Streamlit App")
-
-# Create a slider
-slider_value = st.slider("Select a value", 0, 100, 50)
-
-# Create a chart
-data = pd.DataFrame({"x": [1, 2, 3, 4, 5], "y": [1, 4, 9, 16, 25]})
-fig, ax = plt.subplots()
-ax.plot(data["x"], data["y"])
-st.pyplot(fig)
+st.title("hello world")
